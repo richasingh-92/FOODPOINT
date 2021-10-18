9a5bcbb4ebeb195b401bf849473dd7d29805b5e7
richasingh-92 <91095253+richasingh-92@users.noreply.github.com>
2021-10-19 01:51:54 +0530

    Delete website.js

--- website.js
+++
@@ -1,9 +0,0 @@
-
-alert ("Welcome \n It's my first responsive website. \nSo please ignore the design my main motive is to make it responsive :)")
-
-
-alert ("Any suggestion will be appericiated\n THANKYOU")
-
-
-
-    
